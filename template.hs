29207b352e7115d76af1aeb9c609f42f8cfa290f
edherbert <edwardherbert802@yahoo.com>
2018-03-18 23:10:27 +0000

    Finished the ordering bit and started work on the user interface

--- template.hs
+++ template.hs
@@ -1,3 +1,6 @@
+import Data.List
+import Data.Ord
+
 --
 -- MATHFUN
 -- Template for the Haskell assignment program (replace this comment)
@@ -20,13 +23,13 @@ data Film = Film{
 --A function that returns the testDatabase. It's not really storing it.
 testDatabase :: [Film]
 --testDatabase = [Film "A film" "A director" 1048 [] [], Film "Second" "Another director" 2000 [] []]
-testDatabase = [Film "Blade Runner" "Ridley Scott" 1982 ["Zoe", "Heidi", "Another", "Someone else"] ["Sam"], Film "The Fly" "David Cronenberg" 1986 ["First"] [], Film "Body of Lies" "Ridley Scott" 2008 ["Something"] ["Third", "Zoe"] ]
+testDatabase = [Film "Blade Runner" "Ridley Scott" 1982 ["Zoe", "Heidi", "Another", "Someone else"] ["Sam"], Film "The Fly" "David Cronenberg" 1986 ["First"] [], Film "Body of Lies" "Ridley Scott" 2008 ["Something"] ["Third", "Zoe"]]
 
 --Add a film to the database by taking a list of films and the film to add.
 addFilm :: [Film] -> Film -> [Film]
 addFilm fArray f = f:fArray
 
---List all the films in the database\
+--List all the films in the database
 listFilms = testDatabase
 
 --Get all the films by a certain director
@@ -40,38 +43,178 @@ filmsByDirector (x:xs) d
 totalRatings :: Film -> Int
 totalRatings f = (length $ usersLiked f) + (length $ usersDisliked f)
 
+--Get the website rating for a film.
+--This is the percentage of users liked rounded.
 getWebsiteRating :: Film -> Int
 getWebsiteRating f = round ((fromIntegral (length $ usersLiked f)) / (fromIntegral (totalRatings f)) * 100)
 --getWebsiteRating f = 10 / 5
 
+--Get a list of films with a rating of over 75%
 getFilmsWith75 :: [Film] -> [Film]
 getFilmsWith75 [] = []
 getFilmsWith75 (x:xs)
   | getWebsiteRating x >= 75 = x:getFilmsWith75 xs
   | otherwise = getFilmsWith75 xs
 
+--Get the sumative website ratings for a list of films.
 getSumativeRatings :: [Film] -> Int
 getSumativeRatings [] = 0
 getSumativeRatings (x:xs) = (getWebsiteRating x) + getSumativeRatings xs
 
-averageforDirector :: [Film] -> Int
-averageforDirector x = div (getSumativeRatings x) (length x)
+--Find the average website rating for a director.
+averageWebsiteRatingByList :: [Film] -> Int
+averageWebsiteRatingByList x = div (getSumativeRatings x) (length x)
 
+--Get the average website rating for a particular director.
+averageForDirector :: String -> [Film] -> Int
+averageForDirector director films = averageWebsiteRatingByList (filmsByDirector films director)
+
+--Get a list of the films that that user has either liked or disliked.
 filmsByUser :: String -> [Film] -> [Film]
 filmsByUser _ [] = []
 filmsByUser u (x:xs)
   | elem u (usersLiked x) || elem u (usersDisliked x) = x:filmsByUser u xs
   | otherwise = filmsByUser u xs
 
+
+--By the looks of things these don't return the entire list, just the one film.
+--Add a user to a single film's like list.
 addUserToFilmLikes :: String -> Film -> Film
 addUserToFilmLikes user f = f { usersLiked = user:(usersLiked f) }
 
+--Add the user to the liked list for that film.
 likeFilm :: String -> String -> [Film] -> [Film]
 likeFilm _ _ [] = []
 likeFilm user filmName (x:xs)
   | title x == filmName = (addUserToFilmLikes user x):likeFilm user filmName xs
   | otherwise = likeFilm user filmName xs
 
+
+addUserToFilmDislikes :: String -> Film -> Film
+addUserToFilmDislikes user f = f { usersDisliked = user:(usersDisliked f) }
+
+--Add the user to the liked list for that film.
+--User, Filmname, film list
+dislikeFilm :: String -> String -> [Film] -> [Film]
+dislikeFilm _ _ [] = []
+dislikeFilm user filmName (x:xs)
+  | title x == filmName = (addUserToFilmDislikes user x):dislikeFilm user filmName xs
+  | otherwise = dislikeFilm user filmName xs
+
+--Sort films in decending order.
+
+--Get a list of the films released between two given years.
+filmsReleasedBetween :: Int -> Int -> [Film] -> [Film]
+filmsReleasedBetween _ _ [] = []
+filmsReleasedBetween start end (x:xs)
+  | release x >= start && release x <= end = x:(filmsReleasedBetween start end xs)
+  | otherwise = filmsReleasedBetween start end xs
+
+--Wrap the films list into a tuple containing the website rating.
+--This tuple form can be used to sort the list according to its rating.
+wrapFilms :: [Film] -> [(Film, Int)]
+wrapFilms [] = []
+wrapFilms (x:xs) = (x, getWebsiteRating x):wrapFilms xs
+
+--Remove the tuple from the list
+unwrapFilms :: [(Film, Int)] -> [Film]
+unwrapFilms [] = []
+unwrapFilms (x:xs) = fst x:unwrapFilms xs
+
+--Sort the wrapped films by their second value (the website rating)
+sortByRating :: [(Film, Int)] -> [(Film, Int)]
+sortByRating = sortBy (comparing snd)
+
+--Wrap the films, sort them, then unwrap them.
+sortFinal :: [Film] -> [Film]
+sortFinal films = unwrapFilms (sortByRating (wrapFilms films))
+
+--Get the films between two years ordered in ascending order by website rating.
+filmsBetweenOrdered :: Int -> Int -> [Film] -> [Film]
+filmsBetweenOrdered first second films = reverse (sortFinal (filmsReleasedBetween first second films))
+
+
+formatFilm :: Film -> String
+formatFilm f =
+  "Title:  " ++ title f ++ "\n\
+  \Director:  " ++ director f ++ "\n\
+  \Release: " ++ show (release f) ++ "\n\
+  \Website Rating:  " ++ show (getWebsiteRating f) ++ "\n\
+  \"
+
+formatFilmList :: [Film] -> String
+formatFilmList [] = ""
+formatFilmList (x:xs) = formatFilm x ++ formatFilmList xs
+
+mainMenu = "-------------------------------------------------------------\n\
+            \1. Add a new film to the database.\n\
+            \2. Print all films in the database.\n\
+            \3. Print all the films by a given director.\n\
+            \4. Print films with a website rating of 75% or higher.\n\
+            \5. Print the average rating for a given director.\n\
+            \6. Print the films that a particular user has rated.\n\
+            \7. Like or dislike a film.\n\
+            \8. Print films between two years sorted by website rating.\n\
+            \9. Exit\n\
+            \-------------------------------------------------------------"
+
+processInput :: String -> String
+processInput s
+  | s == "1" = "first"
+  | otherwise = "Something else"
+
+--input
+
+main = do
+    putStrLn "Please enter your name:"
+    name <- getLine
+    putStrLn ("Hey " ++ name ++ "")
+    loop
+
+loop = do
+  putStrLn mainMenu
+  input <- getLine
+  if input == "1"
+    then first
+    else do
+  if input == "2"
+    then second
+    else do
+  if input == "3"
+    then third
+    else do
+  if input == "4"
+    then fourth
+    else do
+  if input == "5"
+    then fifth
+    else do
+    putStrLn "Nothing"
+  loop
+
+test = do
+  putStrLn $ formatFilmList testDatabase
+
+first = do
+    putStrLn "first"
+
+second = do
+    putStrLn "second"
+
+third = do
+    putStrLn "Please enter the director name:"
+    director <- getLine
+    putStrLn $ formatFilmList $ filmsByDirector testDatabase director
+    loop
+
+fourth = do
+    putStrLn $ formatFilmList $ getFilmsWith75 testDatabase
+    loop
+
+fifth = do
+    putStrLn "Please enter the director name:"
+    director <- getLine
+    putStrLn $ show $ averageForDirector director testDatabase
 --Films as string function which returns a film formatted in a readable form.
 
 -- Demo function to test basic functionality (without persistence - i.e.
